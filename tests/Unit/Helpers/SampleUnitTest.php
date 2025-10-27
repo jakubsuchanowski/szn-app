8579838cc01d5ed9be545e7b3448ba810a62a9f8
jakubsuchanowski <kubasuchanowski@gmail.com>
2025-10-27 21:56:19 +0100

    naprawa bledu z testami

---
+++ tests/Unit/Helpers/SampleUnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleUnitTest extends TestCase
+{
+    public function test_basic_math()
+    {
+        $this->assertEquals(4, 2 + 2);
+    }
+}
